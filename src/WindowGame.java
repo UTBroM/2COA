36fb55d587e5cfe34b61bd5be1906913e0a4be22
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-05-22 15:23:52 +0200

    Projet Slick2D initial

---
+++ src/WindowGame.java
@@ -0,0 +1,37 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class WindowGame extends BasicGame {
+    private GameContainer container;
+
+	public WindowGame() {
+        super("Lesson 1 :: WindowGame");
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        this.container = container;
+    }
+
+    public void render(GameContainer container, Graphics g) throws SlickException {
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+    }
+    
+    @Override
+    public void keyReleased(int key, char c) {
+        if (Input.KEY_ESCAPE == key) {
+            container.exit();
+        }
+    }
+    
+    public static void main(String[] args) throws SlickException {
+        new AppGameContainer(new WindowGame(), 640, 480, false).start();
+    }
+}
