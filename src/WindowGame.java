b25e3ab292c23d34d0c484acb61d072f409f100b
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-05-22 17:45:02 +0200

    Créer un carré avec 2048 dedans

--- src/WindowGame.java
+++ src/WindowGame.java
@@ -4,10 +4,15 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.ShapeFill;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class WindowGame extends BasicGame {
     private GameContainer container;
+    
+    private Rectangle carre;
+    private Color carreCouleur;
 
 	public WindowGame() {
         super("Lesson 1 :: WindowGame");
@@ -16,11 +21,17 @@ public class WindowGame extends BasicGame {
     @Override
     public void init(GameContainer container) throws SlickException {
         this.container = container;
+        carre = new Rectangle(200, 100, 100, 100);
+        carreCouleur = new Color(255, 0, 0);
     }
 
     public void render(GameContainer container, Graphics g) throws SlickException {
     	Color background = new Color(255, 128, 0);
     	g.setBackground(background);
+    	g.setColor(carreCouleur);
+    	g.fill(carre);
+    	g.setColor(Color.white);
+    	g.drawString("2048", carre.getCenterX()-20, carre.getCenterY()-10);
     }
 
     @Override
