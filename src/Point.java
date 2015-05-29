b52134f73a2bb3157fb9c6d0b11217cb2895c4c9
Victor SENE <victor@sene.ovh>
2015-05-29 13:36:11 +0200

    private final pour les coordonées des points de bases

--- src/Point.java
+++ src/Point.java
@@ -1,12 +1,20 @@
 
 public class Point {
-	int x;
-	int y;
+	private final int x;
+	private final int y;
 	
 	public Point(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 		//System.out.println("x=" + x + "\ty=" + y);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
 	}
 }
