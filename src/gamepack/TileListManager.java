6df6e17be4943b0385ff8eab6f1bc537ae3f38fa
Victor SENE <victor@sene.ovh>
2015-05-31 18:01:49 +0200

    Gestion basique tileList

--- src/gamepack/TileListManager.java
+++ src/gamepack/TileListManager.java
@@ -1,5 +1,18 @@
 package gamepack;
 
+import java.util.Iterator;
+
 public class TileListManager {
+	private TileList tileList;
+
+	public TileList getTileList() {
+		return tileList;
+	}
+	public void setTileList(TileList tileList) {
+		this.tileList = tileList;
+	}
+	
+	
+	
 	
 }
