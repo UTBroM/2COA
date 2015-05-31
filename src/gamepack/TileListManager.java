fc9c3a5d6b2e9796b360ae710c6c5f3542b3e4a5
Victor SENE <victor@sene.ovh>
2015-05-31 20:05:36 +0200

    Création de Fusion (je pense qu'on peut encore améliorer #24)

--- src/gamepack/TileListManager.java
+++ src/gamepack/TileListManager.java
@@ -12,5 +12,18 @@ public class TileListManager {
 		this.tileList = tileList;
 	}
 	
-	
+	public void fusion(){
+		for (int i = 0; i < tileList.getSize(); i++) 
+		{
+			for (int j = 0; j < tileList.getSize(); j++) 
+			{
+				if (tileList.getTile(i).getCenterX() == tileList.getTile(j).getX() && tileList.getTile(i).getCenterY() == tileList.getTile(j).getY())
+				{
+					tileList.setTile(i,(int)tileList.getTile(i).getX(), (int)tileList.getTile(i).getY(), (tileList.getTile(i).getValue()) * 2);
+					// Faut pas appeler un listener là ?
+					tileList.remove(j);
+				}
+			}	
+		}
+	}
 }
