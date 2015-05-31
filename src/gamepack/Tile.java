1c73059a6f0c60e3c3927929b5aeb1a7c8a12b09
Victor SENE <victor@sene.ovh>
2015-05-31 17:45:23 +0200

    Ajout et gestion arrivedPoint. Closed #15

--- src/gamepack/Tile.java
+++ src/gamepack/Tile.java
@@ -3,18 +3,18 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 
 
-public class Tile implements DrawableObject//implements DrawableObject
+public class Tile implements DrawableObject
 {
 	private int value;
 	private Rectangle rectangle;
 	private Direction tileDirection;	//Indique dans quelle direction va le tile
+	private Point arrivedPoint;			//Represent the point where the tile will gone
 	
 	
 	public Tile(int x, int y)
 	{
 		this(x,y,2); //changer  le 2 en random value entre 2 ou 4 ou bombe
 	}
-	
 	public Tile(int x, int y, int value)
 	{
 		this.rectangle = new Rectangle(x, y, 140, 140); 
@@ -26,14 +26,6 @@ public class Tile implements DrawableObject//implements DrawableObject
 	{
 		return this.value;
 	}
-	
-	
-	public void move(float x, float y)
-	{
-		this.rectangle.setX(this.rectangle.getX()+x);
-		this.rectangle.setY(this.rectangle.getY()+y);
-	}
-	
 	public float getCenterX()
 	{
 		return this.rectangle.getCenterX();
@@ -41,6 +33,20 @@ public class Tile implements DrawableObject//implements DrawableObject
 	public float getCenterY()
 	{
 		return this.rectangle.getCenterY();
+	}
+	public Point getArrivedPoint()
+	{
+		return this.arrivedPoint;
+	}
+	public void setArrivedPoint(Point p)
+	{
+		this.arrivedPoint = p;
+	}
+	
+	public void move(float x, float y)
+	{
+		this.rectangle.setX(this.rectangle.getX()+x);
+		this.rectangle.setY(this.rectangle.getY()+y);
 	}
 	
 	public void beDrawn(Graphics g)
