9394c5aa914e109b9927d13d1b10f54068c618e2
Victor SENE <victor@sene.ovh>
2015-06-08 17:25:13 +0200

    création de la classe bomb

---
+++ src/gamepack/Bomb.java
@@ -0,0 +1,23 @@
+package gamepack;
+
+import org.newdawn.slick.geom.Rectangle;
+
+public class Bomb extends Tile{
+	private int tempsExplosion;
+	private int rayonExplosion;
+	
+	public Bomb(int x, int y, int value, int size, int tempsExplosion, int rayonExplosion)
+	{
+		super(x, y, value, size);
+		this.tempsExplosion = tempsExplosion;
+		this.rayonExplosion = rayonExplosion;
+	}
+	
+	public Bomb(int x, int y, int size, int tempsExplosion)
+	{
+		super(x,y,size);
+		this.tempsExplosion = tempsExplosion;
+		this.rayonExplosion = rayonExplosion;
+	}
+	
+}
